c9449d65dc1a6ffd179c5b7d6f5512236558049b
Ericson <palisocericson87@gmail.com>
2024-11-11 18:54:45 +0800

    updated

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -6,12 +6,14 @@ const poppins = Poppins({
 })
 
 export default function Page() {
-  ;<div className={`${poppins.className} flex ml-36 mb-10`}>
-    <div className='flex flex-col text-center w-full items-center'>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ipsa,
-      officia corporis officiis quia ad, aliquid consectetur consequuntur sed
-      debitis rerum, cum molestias deleniti error soluta dolore quaerat?
-      Voluptate, illo.
+  return (
+    <div className={`${poppins.className} flex ml-36 mb-10`}>
+      <div className='flex flex-col text-center w-full items-center'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ipsa,
+        officia corporis officiis quia ad, aliquid consectetur consequuntur sed
+        debitis rerum, cum molestias deleniti error soluta dolore quaerat?
+        Voluptate, illo.
+      </div>
     </div>
-  </div>
+  )
 }
